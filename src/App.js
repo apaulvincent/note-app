b67354908af692cfe89c2221f80f12a1dc721682
Paul De Ocampo <paul.deocampo@positivelendingsolutions.com.au>
2018-07-13 15:25:45 +0800

    setup redux and rounter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Home from './Home';
-import About from './About';
-import Topics from './Topics';
+import Home from './views/Home';
+import About from './views/About';
+import Topics from './views/Topics';
+import Menu from './components/Menu';
 
 const App = () => (
   <Router>
-  <div>
 
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/topics" component={Topics} />
-  
+    <div>
+      <Menu/>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />  
     </div>
 </Router>
 );
